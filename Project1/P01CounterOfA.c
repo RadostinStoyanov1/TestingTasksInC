88a9b97f115cd7f0ef38d4a37f79f4437724c03f
RadostinStoyanov <Radostin_Stoyanov@students.softuni.bg>
2025-05-09 20:59:13 +0300

    Finished Task 1

--- Project1/P01CounterOfA.c
+++ Project1/P01CounterOfA.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+
+#define LENGTH 256
+
+int findCountOf_a_Char(char *input) {
+    int i, count = 0;
+
+    for (i = 0; i < LENGTH; i++) {
+        if (input[i] == 'a') {
+            count++;
+        }
+    }
+    
+    return count;
+}
 
 int main() {
 
-    char name[] = "John Doe";
-    printf("Hello World! %s\n", name);
+    char input[LENGTH];
+    printf("\nEnter a string up to %d characters\n", LENGTH);
+    fgets(input, sizeof(input), stdin);
+
+    int aCount = findCountOf_a_Char(input);
+    printf("Character 'a' was met %d times\n", aCount);
 
     return 0;
 }
